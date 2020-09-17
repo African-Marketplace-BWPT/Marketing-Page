8a34073d6f3f48c5489e63c1d7968b0c06064b57
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-09-16 22:26:26 -0400

    added reviews section functionality on script.js

---
+++ script.js
@@ -0,0 +1,70 @@
+// Reviews functionality 
+
+const reviews = [
+  {
+    id: 1,
+    name: "Anakin Skywalker",
+    job: "CEO of The Jedi",
+    img: "images/anakin.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat doloribus architecto, itaque corrupti rerum iusto. Placeat doloribus architecto."
+},
+  {
+  id: 2,
+  name: "Obi-Wan Kanobi",
+  job: "Chairman of The Force",
+  img: "images/obiwan.png",
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat doloribus architecto, itaque corrupti rerum iusto. Sit amet consectetur adipisicing elit."
+},
+  {
+  id: 3,
+  name: "Rex",
+  job: "CEO of The Clones",
+  img: "images/rex.png",
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat doloribus architecto, itaque corrupti rerum iusto."
+}
+];
+
+// review - select items
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load intial item
+window.addEventListener('DOMContentLoaded', function(){
+  showPerson(currentItem);
+});
+
+// show person based on item
+function showPerson(person){
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', function() {
+  currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener('click', function() {
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
